a170d6faf530d23b8fe124314bfedbd6e969b508
0minKoh <esesang13@naver.com>
2022-03-27 15:40:41 +0900

    프로젝트 시작

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- Remove & use example component! -->
+  <HelloWorld 
+    :msg="message" 
+    @click="handler" />
+  <HelloWorld msg="Good" />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() { //data: function () {} 의 축약형
+    return {
+      message: 'Hello Vue!' //variable: 값 // 변수 선언 방식
+    }
+  },
+  methods: {
+    handler() {
+      console.log(this.message)
+      this.message += '!'
+    }
+  }
+}
+</script>
