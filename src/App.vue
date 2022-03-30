212ae5ee9e1d606af80490785520088e9ce0fc66
0minKoh <esesang13@naver.com>
2022-03-30 11:27:33 +0900

    라이프사이클과 api

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <h1 @click="increase">
+    {{ count }} / {{ doubleCount }}
+  </h1>
+  <h1 @click="ChangeMessage">
+    {{ message }} / {{ reversedMessage }}
+  </h1>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() { //data: function () {} 의 축약형
+  data() {
     return {
-      message: 'Hello Vue!' //variable: 값 // 변수 선언 방식
+      count: 0,
+      message: 'Hello World!'
     }
   },
+  //computed
+  //계산한 후 return을 받을 때 사용하는 옵션
+  computed: {
+    doubleCount() {
+      return this.count * 2
+    },
+    reversedMessage() {
+      return this.message.split('').reverse().join('')
+    },
+  },
+
+  watch: {
+    message(newValue) {
+      console.log(newValue)
+    }
+  },
+  //created() 
+  //html 부분과 연결되기 전에 실행
+  created() {
+    console.log(this.message)
+  },
+  //mounted()
+  //html 부분과 연결된 후 실행
+  mounted() {
+    console.log(this.count)
+  },
+
   methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+    increase() {
+      this.count += 1
+    },
+    changeMessage() {
+      this.message = "Good?!"
     }
   }
 }
---
+++ src/components/reponsive.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1 @click="increase">
+    {{ count }} / {{ doubleCount }}
+  </h1>
+  <h1 @click="ChangeMessage">
+    {{ message }} / {{ reversedMessage }}
+  </h1>
+</template>
+
+<script>
+import { ref, computed, watch, onMounted, } from 'vue'
+
+export default {
+  //setup()은 html부분과 아직 연결되기 전에 실행된다
+  setup() {
+    const count = ref(0)
+    function increase() {
+      count.value += 1
+    }
+    const doubleCount = computed(() => {
+      return count.value * 2
+    })
+
+    
+    const message = ref('Hello World!')
+    const reversedMessage = computed(() => {
+      return message.value.split('').reverse().join('')
+    })
+    //watch(추적대상, 실행함수)
+    watch(message, (newValue) => { console.log(newValue) })
+    function changeMessage() {
+      message.value = "Good?!"
+    }
+    console.log(count)
+
+    onMounted(() => {
+      console.log(this.message)       
+    })
+
+    return {
+      count,
+      doubleCount,
+      message,
+      increase,
+      reversedMessage,
+      changeMessage,
+    }
+  }
+}
+</script>
